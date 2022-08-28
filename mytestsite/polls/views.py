49955d06eb27b79037ab6eba112ee88842ee9a16
patbc <culleyp@oregonstate.edu>
2022-08-27 19:48:31 -0500

    add images to newsfeed

--- mytestsite/polls/views.py
+++ mytestsite/polls/views.py
@@ -7,7 +7,17 @@ from django.http import HttpResponse
 
 def index(request):
     response = requests.get('https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology&apikey=RIDUWMSKIS4518PV').json()
-    return render(request, 'polls/content.html', {'response':response})
+    composites = requests.get('https://api.twelvedata.com/price?symbol=AAPL,MSFT,CRWD,GOOG,PLTR&apikey=c486aa4073cd405daae26abe7f2aef04').json()
+    # high_volume = requests.get('https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=a47ede9cfb01fb619982832def1ce5cc').json()
+
+    images = response['feed'][0]['banner_image']
+    # news_image = requests.get(images)    
+    context = {
+        'news': response,
+        # 'image': news_image,
+        'composites': composites,
+    }
+    return render(request, 'polls/content.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
