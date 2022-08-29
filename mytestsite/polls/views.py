ead747bc735f86ed912bfa89abaf2879deccb53d
patbc <culleyp@oregonstate.edu>
2022-08-29 16:29:38 -0500

    add home page trending stocks

--- mytestsite/polls/views.py
+++ mytestsite/polls/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 import requests  
 from .models import Question
 from django.http import HttpResponse
+import plotly 
 # Create your views here.
 
 
 def index(request):
     response = requests.get('https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology&apikey=RIDUWMSKIS4518PV').json()
-    composites = requests.get('https://api.twelvedata.com/price?symbol=AAPL,MSFT,CRWD,GOOG,PLTR&apikey=c486aa4073cd405daae26abe7f2aef04').json()
-    # high_volume = requests.get('https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=a47ede9cfb01fb619982832def1ce5cc').json()
+    composites = requests.get('https://api.twelvedata.com/price?symbol=NVDA,TSLA,AAPL,MSFT,CRWD,GOOG,PLTR&apikey=c486aa4073cd405daae26abe7f2aef04').json()
+    high_volume = requests.get('https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=a47ede9cfb01fb619982832def1ce5cc').json()
 
     images = response['feed'][0]['banner_image']
-    # news_image = requests.get(images)    
     context = {
         'news': response,
-        # 'image': news_image,
         'composites': composites,
+        "hi_volume": high_volume
     }
     return render(request, 'polls/content.html', context)
 
