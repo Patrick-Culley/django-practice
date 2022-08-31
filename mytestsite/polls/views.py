b31a3a99391e0f058afcf49f4e5eb5b108e19529
patbc <culleyp@oregonstate.edu>
2022-08-30 21:10:53 -0500

    add chart.js

--- mytestsite/polls/views.py
+++ mytestsite/polls/views.py
@@ -1,8 +1,14 @@
+from time import time
 from django.shortcuts import render
 import requests  
 from .models import Question
+import pandas
 from django.http import HttpResponse
-import plotly 
+import plotly.graph_objects as go
+import plotly.offline as py
+from plotly.offline import plot
+import plotly.graph_objs as go
+
 # Create your views here.
 
 
@@ -10,14 +16,22 @@ def index(request):
     response = requests.get('https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology&apikey=RIDUWMSKIS4518PV').json()
     composites = requests.get('https://api.twelvedata.com/price?symbol=NVDA,TSLA,AAPL,MSFT,CRWD,GOOG,PLTR&apikey=c486aa4073cd405daae26abe7f2aef04').json()
     high_volume = requests.get('https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=a47ede9cfb01fb619982832def1ce5cc').json()
+    
 
-    images = response['feed'][0]['banner_image']
     context = {
         'news': response,
         'composites': composites,
-        "hi_volume": high_volume
+        "hi_volume": high_volume,
+        "graphs": graphs()
     }
     return render(request, 'polls/content.html', context)
+
+def graphs(): 
+    response = requests.get('https://financialmodelingprep.com/api/v3/historical-chart/30min/AAPL?apikey=a47ede9cfb01fb619982832def1ce5cc').json()
+    price_list = []
+    for value in response: 
+        price_list.append(value["open"])
+    return price_list
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
